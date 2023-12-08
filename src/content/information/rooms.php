7ab6dbea39bb3dc579aef8475d850090be815dfd
“[konrad <[wi21b012@technikum-wien.at]>
2023-12-08 20:30:41 +0100

    edit rooms page

--- src/content/information/rooms.php
+++ src/content/information/rooms.php
@@ -20,8 +20,108 @@
 </header>
 
 <main>
+    <div class="container justify-content-left">
 
-    <img src="../../images/construction_2.jpg" alt="Under Construction...">
+        <div class="dropdown mb-3">
+            <button class="btn btn-outline-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Bitte wählen Sie die Zimmer-Kategorie!
+            </button>
+            <ul class="dropdown-menu">
+                <li><form method="POST"><input name="roomType" type="submit" class="dropdown-item" value="Deluxe Zimmer"/></form></li>
+                <li><form method="POST"><input name="roomType" type="submit" class="dropdown-item" value="Junior Suite"/></li>
+                <li><form method="POST"><input name="roomType" type="submit" class="dropdown-item" value="Signature Suite"/></li>
+                <li><form method="POST"><input name="roomType" type="submit" class="dropdown-item" value="Grand Suite"/></li>
+            </ul>
+        </div>
+
+    <?php
+        if(isset($_POST["roomType"])) {
+            $roomType = htmlspecialchars($_POST["roomType"]);
+            $roomSize;
+            $roomPrize;
+            
+            echo "<h2>$roomType</h2>";
+
+            switch ($roomType) {
+                case "Deluxe Zimmer":
+                    $roomType = "deluxe_room";
+                    $roomSize = "27-39";
+                    $roomPrize = 300;
+                    break;
+                case "Junior Suite":
+                    $roomType = "junior_suite";
+                    $roomSize = "40-55";
+                    $roomPrize = 550;
+                    break;
+                case "Signature Suite":
+                    $roomType = "signature_suite";
+                    $roomSize = "50-65";
+                    $roomPrize = 800;
+                    break;
+                case "Grand Suite":
+                    $roomType = "grand_suite";
+                    $roomSize = "ab 95";
+                    $roomPrize = 1400;
+                    break;
+            }
+
+            echo "<p>Größe: $roomSize m²</p>";
+            echo "<p>Preis: ab $roomPrize € p. P. u. Nacht</p>";
+        }
+    ?>
+
+    <?php if(isset($roomType)) : ?>
+        <div id="carouselExample" class="carousel slide mb-3">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="<?php echo "../../images/rooms/" . $roomType . "_01.jpg"; ?>" class="d-block w-100" alt="Alt-Text">
+                </div>
+                <div class="carousel-item">
+                <img src="<?php echo "../../images/rooms/" . $roomType . "_02.jpg"; ?>" class="d-block w-100" alt="Alt-Text">
+                </div>
+                <div class="carousel-item">
+                <img src="<?php echo "../../images/rooms/" . $roomType . "_03.jpg"; ?>" class="d-block w-100" alt="Alt-Text">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+        </div>
+    <?php endif ?>
+
+    </div>
+
+    <!--
+        <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="..." class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+    </div>
+
+    <img src="../../images/construction_2.jpg" alt="Under Construction...">-->
 
 </main>
 
